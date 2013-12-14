3775a21624f84a770720c0f1de426bf9045620d4
James Black <james@James-Black.com>
2013-12-13 18:30:40 -0700

    Adding some debug info

--- routes/version-check.js
+++ routes/version-check.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
   app.post("/version-check", function(req, res) {
     console.log("Version Check");
 
+    console.log(req.body);
     if (req.body.ref.toLowerCase() !== "refs/heads/master") { return res.send({}); }
     if (req.body.head_commit.modified.indexOf("package.json") === -1) { return res.send({}); }
 
@@ -38,25 +39,25 @@ module.exports = function(app) {
       .then(
         function success() {
 
-          if (old_version !== new_version) {
-            superagent
-              .post(api + "/repos/" + owner + "/" + repo + "/releases")
-              .set('Accept', 'application/json')
-              .set('User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5')
-              .set('Content-Type', 'application/json')
-              .send({
-                "tag_name": new_version,
-                "target_commitish": req.body.after,
-                "name": new_version,
-                "body": req.body.head_commit.message
-              })
-              .auth("jamesblack", "eCgaming1800")
-              .end(function(error, response) {
-                if (error) { console.log(error); }
-                if (!response.ok) { console.log(res.status); }
-              });
+          // if (old_version !== new_version) {
+          //   superagent
+          //     .post(api + "/repos/" + owner + "/" + repo + "/releases")
+          //     .set('Accept', 'application/json')
+          //     .set('User-Agent', 'curl/7.24.0 (x86_64-apple-darwin12.0) libcurl/7.24.0 OpenSSL/0.9.8r zlib/1.2.5')
+          //     .set('Content-Type', 'application/json')
+          //     .send({
+          //       "tag_name": new_version,
+          //       "target_commitish": req.body.after,
+          //       "name": new_version,
+          //       "body": req.body.head_commit.message
+          //     })
+          //     .auth("jamesblack", "eCgaming1800")
+          //     .end(function(error, response) {
+          //       if (error) { console.log(error); }
+          //       if (!response.ok) { console.log(res.status); }
+          //     });
 
-          }
+          // }
           res.send({"old_version": old_version, "new_version": new_version});
         },
         function failure(error) {
